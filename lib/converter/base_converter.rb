5a0b764d4d46bd59b0e1cba08ffe2beacef70ebd
Brendan Thomas <bt331609@gmail.com>
2021-09-21 16:52:51 -0400

    resolve camel keys

--- lib/converter/base_converter.rb
+++ lib/converter/base_converter.rb
@@ -9,7 +9,8 @@ module Converter
     def to_json
       payload = {}
       self.class.fields.map do |f|
-        resolved_field = resolve_data[:data].dig(*f[:from])
+        resolved_field = resolve_data[:data].dig(*camelized_keys(f[:from]))
+
         raise MissingField, "#{f[:name]} can't be nil!" if f[:required] && resolved_field.nil?
 
         payload[f[:name]] = resolved_field
@@ -20,6 +21,12 @@ module Converter
     def resolve_data
       @resolve_data ||=
         Converter::DataResolver.data_for(:id, @arg_id, self.class.maybe).with_indifferent_access
+    end
+
+    private
+
+    def camelized_keys(from)
+      from.map { |field_name| field_name.to_s.camelize(:lower).to_sym }
     end
   end
 
